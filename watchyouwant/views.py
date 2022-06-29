0628d1a0997eecac32aa16b90e45a5e20e14b216
Kat632 <katherine.biggs@mail.com>
2022-06-29 15:29:14 +0000

    fix view code for 500.html

--- watchyouwant/views.py
+++ watchyouwant/views.py
@@ -8,6 +8,8 @@ def handler403(request, exception):
     """ Error Handler 403 - Forbidden """
     return render(request, "errors/404.html", status=403)
 
-def handler500(request, exception):
+def handler500(request):
     """ Error Handler 500 - Server Error """
-    return render(request, "errors/500.html", status=500)
+    response = render(request, "errors/500.html")
+    response.status_code = 500
+    return response
