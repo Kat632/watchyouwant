85235e9fb371a82dc923026ad1199db6801e3e27
Kat632 <katherine.biggs@mail.com>
2022-07-07 10:33:17 +0000

    add a proper view for the contact form

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
+from django.conf import settings
 
 from products.models import Product, Category
+from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -27,7 +32,31 @@ def terms(request):
 
 
 def contact(request):
-    """
-    view to display Contact Us page
-    """
-    return render(request, 'home/contact.html')
+    """ A view to return the contact page """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        subject = request.POST['subject']
+        message = request.POST['message']
+        email = request.POST['email']
+        sender = "Contact request from" + " " + str(email)
+        msg_mail = str(message) + ", " + str(sender)
+        if form.is_valid():
+            send_mail(
+                subject,
+                msg_mail,
+                email,
+                [settings.DEFAULT_FROM_EMAIL, ]
+            )
+            form.save()
+            messages.success(request, 'Your Email has been sent!')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(
+                request,
+                'There Was an Error Trying to Send Email Message,\
+                    Please check your form.'
+            )
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'home/contact.html', context)
