3a087c995dde6425a61edcc9ca4b4334cbcf4c7a
Kat632 <katherine.biggs@mail.com>
2022-07-08 13:26:46 +0000

    set debug and configure databases in settings.py

--- watchyouwant/settings.py
+++ watchyouwant/settings.py
@@ -14,20 +14,27 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
+    """
+    Conditional import to prevent application
+    error if it cannot find env
+    """
     import env
+    # 'env' imported but unused is because the application
+    # only looks for it when it needs it.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['watchyouwant-pp5.herokuapp.com', 'localhost']
 
@@ -124,16 +131,18 @@ WSGI_APPLICATION = 'watchyouwant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
